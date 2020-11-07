341ff40cb36c064ffed6509ff16c52b982a53bc1
SihemBouhenniche <fs_bouhenniche@esi.dz>
2020-11-07 03:21:50 +0100

    add all files

--- arrowAdvisorApi/settings.py
+++ arrowAdvisorApi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'myapi.apps.MyapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
